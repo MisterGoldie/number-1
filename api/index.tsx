0207c298389c099660b91a0eb2c9e3398b003526
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-08-15 10:33:19 -0500

    goldiestoken

--- api/index.tsx
+++ api/index.tsx
@@ -51,18 +51,8 @@ app.frame('/thirdframe', (c) => {
 
 app.frame('/fourthframe', (c) => {
   return c.res({
-    action: '/fifthframe',
-    image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmeKD5Nr9gF21XSmX7d8HWqyEpo1rRgMtXHwmuKUWpk4pe",
-    intents: [
-      <Button>Next</Button>
-    ],
-  })
-})
-
-app.frame('/fifthframe', (c) => {
-  return c.res({
     action: '/',
-    image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmNrrcf6YKcjqkd6jvJcJezwYCK5rRRXftytcsaA1rrApT",
+    image: "https://amaranth-adequate-condor-278.mypinata.cloud/ipfs/QmPY6MGh1n4SRkLUpnpYuebUjwqQbbCw6tc8A53ZVKncbM",
     intents: [
       <Button.Reset>Back</Button.Reset>,
       <Button.Link href="https://polygonscan.com/token/0x3150e01c36ad3af80ba16c1836efcd967e96776e#balances">Polygonscan</Button.Link>,
